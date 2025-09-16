7511b695fb13894f49347a1b91bd34bfc10ca749
Anant Negi <anantnegi8@gmail.com>
2025-09-16 09:22:02 +0530

    init

---
+++ src/Logger.php
@@ -0,0 +1,39 @@
+<?php
+namespace PsModule\Logger;
+
+class Logger
+{
+    private $handle;
+    private $logPath;
+    private $debugEnabled;
+
+    public function __construct($moduleName, $filename = 'info.log')
+    {
+        $this->debugEnabled = \Configuration::get(strtoupper($moduleName) . '_DEBUG_STATUS', true);
+
+        if ($this->debugEnabled) {
+            $logsDir = _PS_MODULE_DIR_ . $moduleName . '/logs/';
+            if (!file_exists($logsDir)) {
+                mkdir($logsDir, 0755, true);
+            }
+
+            $this->logPath = $logsDir . date('Y-m-d') . '_' . $filename;
+            $this->handle = fopen($this->logPath, 'a');
+        }
+    }
+
+    public function write($message)
+    {
+        if ($this->debugEnabled && $this->handle) {
+            $formattedMessage = date('Y-m-d H:i:s') . ' - ' . print_r($message, true) . "\n";
+            fwrite($this->handle, $formattedMessage);
+        }
+    }
+
+    public function __destruct()
+    {
+        if ($this->debugEnabled && $this->handle) {
+            fclose($this->handle);
+        }
+    }
+}
