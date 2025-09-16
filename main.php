73a884b544501b45c6bc30ec921d9cf7db06ab33
Anant Negi <anantnegi8@gmail.com>
2025-09-16 09:39:32 +0530

    fixed issue and added demo main.php

---
+++ main.php
@@ -0,0 +1,17 @@
+<?php
+
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+
+include 'vendor/autoload.php';
+$copy = new PsModule\Logger\Logger('ready');
+
+print_r('<center>================== Start ==================</center>');
+
+if ($copy->write('Test')) {
+    echo "<center>Write Success</center>";
+} else {
+    echo "<center>Something went wrong</center>";
+}
+
+print_r('<center>================== Done ==================</center>');
+?>
--- src/Logger.php
+++ src/Logger.php
@@ -9,13 +9,13 @@ class Logger
 
     public function __construct($moduleName, $filename = 'info.log')
     {
-        $this->debugEnabled = \Configuration::get(strtoupper($moduleName) . '_DEBUG_STATUS', true);
-
+        $this->debugEnabled = \Configuration::get(strtoupper($moduleName) . '_DEBUG_STATUS', null, null, null, true);
+        
         if ($this->debugEnabled) {
-            $logsDir = _PS_MODULE_DIR_ . $moduleName . '/logs/';
-            if (!file_exists($logsDir)) {
+            $logsDir = _PS_MODULE_DIR_ . $moduleName . '/logs/';            
+            if (!is_dir($logsDir)) {
                 mkdir($logsDir, 0755, true);
-            }
+            }            
 
             $this->logPath = $logsDir . date('Y-m-d') . '_' . $filename;
             $this->handle = fopen($this->logPath, 'a');
